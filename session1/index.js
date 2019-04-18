b5a722953be75f804eb2c06f3e7a43d61461f573
quytudk <quytudkk@gmail.com>
2019-04-18 22:18:55 +0700

    quy

---
+++ session1/index.js
@@ -0,0 +1,21 @@
+//1.Tạo file html
+//2.Gõ html, chọn html:5 để hiển thị khung html
+//3.Tạo file js để code js
+//4.Nối 2 file với nhau thông qua script     <script src="index.js"></script>
+//5.Chạy file html
+//6.Inspect(kiểm tra) trên trình duyệt
+//7.Chuyển qua tab Console để xem kết quả
+
+
+//let name= prompt("Enter your name");  // kí tự điền vào hàm prompt("Enter your name") sẽ gán cho name
+//console.log("hello", name);
+
+// let x=prompt ("Enter X");
+// let y=prompt ("Enter Y");
+// console.log(Number(x)+ Number(y));
+
+//loop
+for(let i=0; i<3; i++) {
+    console.log(i);
+    console.log("hello")
+};
